a36779c234ee61873b80e47e3cbdaf430ec182ad
Khalid <Khalid@Moon>
2014-04-03 12:14:02 -0400

    Updated account by adding operation type to the description

--- src/main/java/com/example/ebanking/utils/EmailUtil.java
+++ src/main/java/com/example/ebanking/utils/EmailUtil.java
@@ -1,5 +1,6 @@
 package com.example.ebanking.utils;
 
+import java.util.Date;
 import java.util.Properties;
 
 import javax.mail.Message;
@@ -10,10 +11,26 @@ import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 
+import java.io.UnsupportedEncodingException;
+import javax.activation.DataHandler;
+import javax.activation.DataSource;
+import javax.activation.FileDataSource;
+import javax.mail.BodyPart;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Multipart;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
+ 
+
 public class EmailUtil {
 
     private static final String username = "altoum88@gmail.com";
-    private static final String password = "t4761629";
+    private static final String password = "";
 
     public static void sendMailTLS(String toEmail,String subject,String body) {
 
@@ -84,5 +101,38 @@ public class EmailUtil {
 //		}
 //
 //    }
+    
+    
+    
+     public static void sendEmail(Session session, String toEmail, String subject, String body){
+        try
+        {
+          MimeMessage msg = new MimeMessage(session);
+          //set message headers
+          msg.addHeader("Content-type", "text/HTML; charset=UTF-8");
+          msg.addHeader("format", "flowed");
+          msg.addHeader("Content-Transfer-Encoding", "8bit");
+ 
+          msg.setFrom(new InternetAddress("altoum88@gmail.com", "The Big Boss"));
+ 
+          msg.setReplyTo(InternetAddress.parse("altoum88@gmail.com", false));
+ 
+          msg.setSubject(subject, "UTF-8");
+ 
+          msg.setText(body, "UTF-8");
+ 
+          msg.setSentDate(new Date());
+ 
+          msg.setRecipients(Message.RecipientType.TO, InternetAddress.parse(toEmail, false));
+          System.out.println("Message is ready");
+          Transport.send(msg);  
+ 
+          System.out.println("EMail Sent Successfully!!");
+        }
+        catch (Exception e) {
+          e.printStackTrace();
+        }
+    }
+
 
 }
